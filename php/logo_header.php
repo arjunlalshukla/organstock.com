c1a6f10ce44e142312eba4c22bd9dcd6d78ce5e5
Arjun Shukla <arjunlalshukla@gmail.com>
2018-02-09 12:17:59 -0700

    First Commit

---
+++ php/logo_header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<link rel="stylesheet" href="organstock.com/css/logo_header.css">
+</head>
+<body>
+	<div>
+		<img src="organstock.com/logo.png" alt="organstock.com logo"</img>
+	</div>
+<body>
+</html>
+
