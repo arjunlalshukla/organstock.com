77592a6b52d5de47864fc68e76f1c10ca757f3dd
Arjun Shukla <arjunlalshukla@gmail.com>
2018-02-09 22:12:27 -0700

    Started Header

---
+++ php/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="organstock.com/css/all.css">
+	<link rel="stylesheet" href="organstock.com/css/header.css">
+</head>
+<body>
+	<div id="header_bloc">
+		<div id="logo_bloc">
+			<img class="logo" src="organstock.com/logo.png" alt="logo">
+		</div>
+		<div id="account_bloc">
+			<form action="organstock.com/php/sign_in.php" method="get">
+				<input type="submit" value="Sign In">
+			</form>
+			<form action="organstock.com/php/sign_up.php" method="get">
+				<input type="submit" value="Sign Up">
+			</form>
+		</div>
+	</div>
+</body>
+</html>
